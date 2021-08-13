182ab336b7b486cfcb1f006eb314941d61ba519b
Jyotsna Mehta <jyotsnamehta24@gmail.com>
2021-08-13 21:13:56 +0530

    Completed.

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 var btnTranslate= document.querySelector("#btn-translate");
 var txtInput= document.querySelector("#txt-input");
 var outputDiv= document.querySelector("#output");
-console.log(outputDiv);
+
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input 
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with the server , try again later!!");
+}
 
 function clickHandler(){
-    outputDiv.innerText="nrfkjekf"+txtInput.value
-    };
+   inputText=txtInput.value;
+   fetch(getTranslationURL(inputText))
+   .then(response => response.json())
+   .then(json =>{
+       var translatedText=json.contents.translated;
+       outputDiv.innerText= translatedText;
+   })
+   .catch(errorHandler)
+   
+};
 
-btnTranslate.addEventListener("click", clickHandler) 
+btnTranslate.addEventListener("click",clickHandler)
+
